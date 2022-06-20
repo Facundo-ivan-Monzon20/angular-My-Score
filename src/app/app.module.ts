77bb54b5aeb48d3f84ff88e1f55d7b2d0876b5d1
Facundo-ivan-Monzon20 <fimonzon@mail.austral.edu.ar>
2022-06-19 22:43:07 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
 import { CartService } from './cart.service';
